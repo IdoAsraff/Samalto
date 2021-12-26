b21946d9033c56bb16732559447e276ebb5f7ab9
Tal Levy <talevy3129@gmail.com>
2021-12-26 16:22:53 +0200

    Added main and sched start activites, base colors

---
+++ app/src/main/java/com/talido/samalto/ScheduleStartActivity.kt
@@ -0,0 +1,11 @@
+package com.talido.samalto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScheduleStartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_schedule_start)
+    }
+}
