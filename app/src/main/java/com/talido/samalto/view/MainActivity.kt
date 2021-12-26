ffe2b4774fb3b2108cc22e629765d32cfbfab1bd
doengo <ido1.asraff@gmail.com>
2021-12-26 16:10:04 +0200

    move main activity to view

--- app/src/main/java/com/talido/samalto/view/MainActivity.kt
+++ app/src/main/java/com/talido/samalto/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.talido.samalto
+package com.talido.samalto.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.talido.samalto.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
